2001d6213604e6e49c2e5a333ca18231482bc267
ILABILI <baillyilham943@gmail.com>
2026-07-21 14:01:52 +0100

    REACT.TEST

---
+++ myapp/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Header from './Header';      
+import Home from './Home';
+import Futer from './Futer';
+function App() {
+  return (
+    <div >
+    <Header/>
+    <Home/> 
+    <Futer/>
+
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/Futer.js
@@ -0,0 +1,13 @@
+function Futer (){
+    return(
+        <div>
+            <footer/>
+             <p>Author: Hege Refsnes</p>
+              <a href="mailto:hege@example.com">hege@example.com</a>
+            
+
+        </div>
+
+    )
+}
+export default Futer
